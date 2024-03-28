ba9daf3c39d0cb4cf8a61500c254c5a615f57acb
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-03-28 11:10:34 -0300

    criado menu mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,40 +6,43 @@ import { IoMdClose } from "react-icons/io";
 
 export default function Header() {
 
-    const abrirMenu = document.getElementById("abrir")
-    const fecharMenu = document.getElementById("fechar")
-    const menu = document.querySelector("ul")
-
     const abrir = ()=>{
-        abrirMenu.style.display = "none"
-        fecharMenu.style.display = "block"
-        menu.classList.remove("max-sm:hidden")
+        const AbrirMenu = document.querySelector("#abrir")
+        const FecharMenu = document.querySelector("#fechar")
+        const Menu = document.querySelector("#menu")
+        AbrirMenu.classList.add("max-sm:hidden")
+        FecharMenu.classList.remove("max-sm:hidden")
+        Menu.classList.remove("max-sm:hidden")
     }
 
     const fechar = ()=>{
-        abrirMenu.style.display = "block"
-        fecharMenu.style.display = "none"
-        menu.classList.add("max-sm:hidden")
+        const AbrirMenu = document.querySelector("#abrir")
+        const FecharMenu = document.querySelector("#fechar")
+        const Menu = document.querySelector("#menu")
+        AbrirMenu.classList.remove("max-sm:hidden")
+        FecharMenu.classList.add("max-sm:hidden")
+        Menu.classList.add("max-sm:hidden")
     }
+
 
     return (
         <header className="container mx-auto px-4 h-24 flex items-center justify-between relative">
             <div className="font-black text-2xl text-slate-600">Logo</div>
             <nav>
                 <ul id="menu" className="flex gap-5 max-sm:hidden max-sm:flex-col max-sm:items-center max-sm:justify-center max-sm:absolute max-sm:top-24 max-sm:right-0 max-sm:bg-gray-50 max-sm:z-10 max-sm:p-4 max-sm:gap-10 max-sm:w-2/5">
-                    <li className="text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer">Como reservar</li>
-                    <li className="text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer">Sobre nós</li>
-                    <li className="text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer">Informações</li>
-                    <li className="text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer">Contato</li>
+                    <li className="text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer" onClick={()=>fechar()}>Como reservar</li>
+                    <li className="text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer" onClick={()=>fechar()}>Sobre nós</li>
+                    <li className="text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer" onClick={()=>fechar()}>Informações</li>
+                    <li className="text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer" onClick={()=>fechar()}>Contato</li>
                 </ul>
 
-                <buttom id="abrir" onClick={()=>abrir()} className="text-2xl text-gray-500 sm:hidden cursor-pointer z-10">
+                <button id="abrir" className="text-2xl text-gray-500 sm:hidden cursor-pointer" onClick={()=>abrir()}>
                     <RiMenu3Line />
-                </buttom>
+                </button>
 
-                <buttom id="fechar" onClick={()=>fechar()} className="text-3xl text-gray-500 sm:hidden max-sm:hidden cursor-pointer z-10">
+                <button id="fechar" className="text-3xl text-gray-500 sm:hidden max-sm:hidden cursor-pointer" onClick={()=>fechar()}>
                     <IoMdClose />
-                </buttom>
+                </button>
             </nav>
         </header>
     )
--- src/components/main/formProcurar/FormProcurar.jsx
+++ src/components/main/formProcurar/FormProcurar.jsx
@@ -6,12 +6,14 @@ export default function FormProcurar() {
     return (
         <form className="space-x-4 mt-7 flex items-center justify-center max-sm:flex max-sm:flex-col max-sm:items-center max-sm:justify-center max-sm:space-x-0 gap-4" action="">
 
-            <select className="bg-gray-50 px-3 py-2 border border-gray-300 rounded text-gray-500">
+            <select className="bg-gray-50 px-3 py-2 border border-gray-300 rounded text-gray-500" required>
                 <option value="">Escolha o estado</option>
+                <option value="">Ceará</option>
             </select>
 
-            <select className="bg-gray-50 px-3 py-2 border border-gray-300 rounded text-gray-500">
+            <select className="bg-gray-50 px-3 py-2 border border-gray-300 rounded text-gray-500" required>
                 <option value="">Escolha a cidade</option>
+                <option value="">Fortaleza</option>
             </select>
 
             <button className="font-medium bg-yellow-500 px-3 py-2 rounded text-yellow-900 flex items-center justify-center gap-1"><IoMdSearch />Pesquisar resorts</button>
